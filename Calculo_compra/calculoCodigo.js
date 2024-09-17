26d1c21ebd4f3115b60a55e35b1437dd1ef9ff7f
JuanLBressanim <juanlbressanim@gmail.com>
2024-09-16 22:27:46 -0300

    Código em andamento

--- Calculo_compra/calculoCodigo.js
+++ Calculo_compra/calculoCodigo.js
@@ -1,16 +1,16 @@
+var valorProdu=parseFloat(prompt( "Qual o valor do seu produto?:" ));
+var quantidadeProdu=parseInt(prompt(" Digite a quantidade comprada: "));
+
 var formaPagamen="   Escolha a forma de pagamento:        \n"; 
 
-formaPagamen=formaPagamen+"codigo | formas de pagamento | dinehiro/cheque | desconto/juros \n"
-
-var formaPagamen=formaPagamen+=" 1 | À vista    | dinheiro ou cheque | 10% de desconto \n ";
-var formaPagamen=formaPagamen+=" 2 | À vista    | dinheiro ou cheque | 5% de desconto \n ";
-var formaPagamen=formaPagamen+=" 3 | Em 2 vezes | cheque             | sem juros \n ";
-var formaPagamen=formaPagamen+=" 4 | Em 3 vezes | cheque             | 10% de juros \n ";
-var formaPagamen=+" \nEscolha a sua forma de pagamento: ";
+formaPagamen+="codigo | formas de pagamento | desconto/juros \n"
+formaPagamen+=" 1 | À vista, dinheiro ou cheque | 10% de desconto \n ";
+formaPagamen+=" 2 | À vista, dinheiro ou cheque | 5% de desconto \n ";
+formaPagamen+=" 3 | Em 2 vezes, cheque          | sem juros \n ";
+formaPagamen+=" 4 | Em 3 vezes, cheque          | 10% de juros \n ";
+formaPagamen=+" \nEscolha a sua forma de pagamento: ";
 
 var escolhaPagamento=parseInt(prompt(formaPagamen));
-var valorProdu=parseFloat(prompt( "Qual o valor do seu produto?:" ));
-var quantidadeProdu=parseInt(prompt(" Digite a quantidade escolhida: "));
 
 if(escolhaPagamento==1){
     var valorQuanti1=valorProdu*quantidadeProdu;
