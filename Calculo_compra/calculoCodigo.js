a03dfe116b32e46b622c197b4dbd7bb897723c4e
JuanLBressanim <juanlbressanim@gmail.com>
2024-09-18 19:58:06 -0300

    andamento codigo

--- Calculo_compra/calculoCodigo.js
+++ Calculo_compra/calculoCodigo.js
@@ -1,28 +1,29 @@
 var valorProdu=parseFloat(prompt( "Qual o valor do seu produto?:" ));
 var quantidadeProdu=parseInt(prompt(" Digite a quantidade comprada: "));
 
-var formaPagamen="   Escolha a forma de pagamento:        \n"; 
+var formaPagamento="   Escolha a forma de pagamento:      \n";
 
-formaPagamen+="codigo | formas de pagamento | desconto/juros \n"
-formaPagamen+=" 1 | À vista, dinheiro ou cheque | 10% de desconto \n ";
-formaPagamen+=" 2 | À vista, dinheiro ou cheque | 5% de desconto \n ";
-formaPagamen+=" 3 | Em 2 vezes, cheque          | sem juros \n ";
-formaPagamen+=" 4 | Em 3 vezes, cheque          | 10% de juros \n ";
-formaPagamen=+" \nEscolha a sua forma de pagamento: ";
+formaPagamento="Código | Formas de pagamento | Desconto/Juros \n ";
+formaPagamento+=" 1 | À vista, dinheiro ou cheque | 10% de desconto \n ";
+formaPagamento+=" 2 | À vista, dinheiro ou cheque | 5% de desconto \n ";
+formaPagamento+=" 3 | Em 2 vezes, cheque          | sem juros \n ";
+formaPagamento+=" 4 | Em 3 vezes, cheque          | 10% de juros \n";
+formaPagamento+="\nEscolha a sua forma de pagamento: ";
 
-var escolhaPagamento=parseInt(prompt(formaPagamen));
+var escolhaPagamento=parseInt(prompt(formaPagamento));
 
 if(escolhaPagamento==1){
     var valorQuanti1=valorProdu*quantidadeProdu;
     var contaPorcentagem1=10/100
-    var valorTotal1=valorProdu*contaPorcentagem1SS
-    
-    console.log(" O valor do seu produto é: " +" " +valorTotal1 );
+    var valorDesconto1=valorProdu*contaPorcentagem1
+    var valorTotalProduto=valorProdu-valorDesconto1
+    console.log(" Seu desconto é de: " +" " +valorDesconto1 +"% ");
+    console.log("O valor total do seu produto é:" +" " +"R%"+valorTotalProduto);
 
 }else if(escolhaPagamento==2){
     var valorQuanti2=valorProdu*quantidadeProdu;
     var contaPorcentagem2=5/100
-    var valorTotal2=valorProdu*contaPorcentagem1
+    var valorTotal2=valorProdu*contaPorcentagem2
     
     console.log(" O valor do seu produto é: " +" " +valorTotal2 );
 
