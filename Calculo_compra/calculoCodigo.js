4bc997d64778bd05a6571ccc2f7838bf2cd48596
JuanLBressanim <juanlbressanim@gmail.com>
2024-09-18 22:26:50 -0300

    Calculo compra em andamento

--- Calculo_compra/calculoCodigo.js
+++ Calculo_compra/calculoCodigo.js
@@ -1,4 +1,4 @@
-var valorProdu=parseFloat(prompt( "Qual o valor do seu produto?:" ));
+var valorProdu=parseFloat(prompt( "Qual o valor do seu produto?: "));
 var quantidadeProdu=parseInt(prompt(" Digite a quantidade comprada: "));
 
 var formaPagamento="   Escolha a forma de pagamento:      \n";
@@ -11,30 +11,45 @@ formaPagamento+=" 4 | Em 3 vezes, cheque          | 10% de juros \n";
 formaPagamento+="\nEscolha a sua forma de pagamento: ";
 
 var escolhaPagamento=parseInt(prompt(formaPagamento));
+var valorTotal=0
 
 if(escolhaPagamento==1){
     var valorQuanti1=valorProdu*quantidadeProdu;
     var contaPorcentagem1=10/100
     var valorDesconto1=valorProdu*contaPorcentagem1
-    var valorTotalProduto=valorProdu-valorDesconto1
-    console.log(" Seu desconto é de: " +" " +valorDesconto1 +"% ");
-    console.log("O valor total do seu produto é:" +" " +"R%"+valorTotalProduto);
+    var valorTotal=valorProdu-valorDesconto1
+    
+    console.log(" O desconto é de: " +" " +valorDesconto1 +"% ");
+    
+    console.log(" O valor total da sua compra é:" +" " +"R$"+valorTotal);
 
 }else if(escolhaPagamento==2){
     var valorQuanti2=valorProdu*quantidadeProdu;
     var contaPorcentagem2=5/100
-    var valorTotal2=valorProdu*contaPorcentagem2
+    var valorDesconto2=valorProdu*contaPorcentagem2
+    var valorTotal=valorProdu-valorDesconto2
+
+    console.log(" O desconto é de: " +" " +valorDesconto2 +"% ");
     
-    console.log(" O valor do seu produto é: " +" " +valorTotal2 );
+    console.log(" A sua compra foi efetuada no total de" +" " +"R%"+valorTotal +" ");
 
 }else if(escolhaPagamento==3){
     var valorQuanti3=valorProdu*quantidadeProdu;
-    var quantiVezes3=valorQuanti3/2
-    var valoTotal3=quantiVezes3-valorQuanti3
+    var quantiVezes3=valorQuanti3/2;
+    var valorTotal=valorQuanti3-quantiVezes3;
     
-    console.log(" O valor do seu produto é: " +" " +valorQuanti3);
+    console.log(" A sua compra foi efetuada em 2x de" +" " +"R$"+valorTotal +", " +"sem juros ");
+
+}else if(escolhaPagamento==4){
+    var valorQuanti4=valorProdu*quantidadeProdu;
+    var valorJuros=(valorQuanti4/3*10);
+    var valorTotal=valorQuanti4+valorJuros;
+
+    console.log(" O juros é de: " +" " +valorJuros +"% ");
+    
+    console.log(" O valor total da sua compra é:" +" " +"R$"+valorTotal);
 
 }else{
-    console.log(" Digite os valores corretamente");
+    console.log(" Digite os valores corretamente ");
 }
 
